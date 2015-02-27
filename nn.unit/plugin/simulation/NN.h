be02260a4a45fdf88c79834e85c1e1754fde8894
gjke <sergii.poluektov@gmail.com>
2015-02-27 15:03:02 +0100

    NN

--- nn.unit/plugin/simulation/NN.h
+++ nn.unit/plugin/simulation/NN.h
@@ -9,10 +9,15 @@ class NN : public plugin::simulation::Unit
 {
 public:
 	NN (plugin::simulation::UnitDelegate& del);
-
 	void step (void);
 
 private:
+	struct fann* ann;
+	bool init;
+	//void initNN();
+	fann_type* in; 
+	fann_type* out;
+	fann_type* out_past;
 };
 
 #endif // _NN_H_
